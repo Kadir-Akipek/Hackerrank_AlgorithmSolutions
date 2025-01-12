41dc2893a8837f9d08df334e99b473fc67fed810
Kadir Akipek <akipekkadir23@gmail.com>
2025-01-12 16:23:28 +0300

    Solution

---
+++ Insertion Sort - Part 1.py
@@ -0,0 +1,12 @@
+def insertionSort1(n, arr):
+    key = arr[-1]
+    i = n - 1
+    
+    while i > 0 and arr[i - 1] > key:
+        arr[i] = arr[i - 1]
+        print(*arr)
+        i -= 1
+    arr[i] = key
+    print(*arr)
+        
+print(insertionSort1(5, [1,2,4,5,3]))
